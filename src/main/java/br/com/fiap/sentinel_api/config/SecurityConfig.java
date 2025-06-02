3727559df272e8acc6b7c86bee5c349fc6e77945
Melissa <melissabsouzaa@gmail.com>
2025-06-02 19:45:53 -0300

    feat: access denied page

--- src/main/java/br/com/fiap/sentinel_api/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/sentinel_api/config/SecurityConfig.java
@@ -33,11 +33,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/register").permitAll()
+                        .requestMatchers("/login", "/register", "/access-denied").permitAll()
                         .requestMatchers("/shelters/edit/**", "/shelters/delete/**", "/shelters/new", "/shelters/save").hasAnyRole("ADMIN", "VOLUNTEER")
                         .requestMatchers("/shelters").hasAnyRole("USER", "ADMIN", "VOLUNTEER")
                         .requestMatchers("/").authenticated()
                         .anyRequest().denyAll()
+                )
+
+                .exceptionHandling(exception -> exception
+                        .accessDeniedPage("/access-denied")
                 )
 
                 .userDetailsService(customUserDetailsService)
--- src/main/java/br/com/fiap/sentinel_api/controller/ErrorController.java
+++ src/main/java/br/com/fiap/sentinel_api/controller/ErrorController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
